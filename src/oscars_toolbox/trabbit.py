1e7455d3f4e09e73e9d9f214e39a795bda5cc585
oscars47 <scholinoscar@gmail.com>
2023-12-23 03:15:47 -0800

    version 0.0.4

--- src/oscars_toolbox/trabbit.py
+++ src/oscars_toolbox/trabbit.py
@@ -3,17 +3,19 @@
 import numpy as np
 from scipy.optimize import minimize, approx_fprime 
 
-def trabbit(loss_func, random_gen, x0_ls=None, num = 1000, alpha=0.3, frac = 0.1, tol = 1e-5, verbose=False):
-    '''Function to implement my custom gradient descent algorithm, trabbit. Goal is to perform double optimization
+def trabbit(loss_func, random_gen, bounds = None, x0_ls=None, num = 1000, alpha=0.3, temperature = 0.1, tol = 1e-5, grad_step = 1e-8, verbose=True):
+    '''Function to implement my custom gradient descent algorithm, trabbit. Goal is to perform double optimization to determine best params.
 
     Parameters:
     :loss_func: function to minimize. assumes all arguments already passed through partial.
     :random_gen: function to generate random inputs
+    :bounds: bounds for each parameter. if None, then no bounds implemented
     :x0_ls: initial guess within a list. if None, then random_gen is used. can also be a list, in which case all initial params will be tried before implementing gd
     :N: number of iterations
     :alpha: learning rate
-    :frac: fraction of iterations to use for rabbit (to hop out and use new random input)
+    :temperature: fraction of iterations to use for rabbit (to hop out and use new random input)
     :tol: tolerance for convergence. if loss is less than tol, then stop
+    :grad_step: size of steps to estimate gradient
     :verbose: whether to print out loss at each iteration
 
     Returns:
@@ -29,7 +31,10 @@ def trabbit(loss_func, random_gen, x0_ls=None, num = 1000, alpha=0.3, frac = 0.1
             :return_param: whether to return params or not
         
         '''
-        result = minimize(loss_func, x)
+        if bounds is not None:
+            result = minimize(loss_func, x, bounds=bounds)
+        else:
+            result = minimize(loss_func, x)
         return result.x
      
     # try initial guesses #
@@ -54,13 +59,13 @@ def trabbit(loss_func, random_gen, x0_ls=None, num = 1000, alpha=0.3, frac = 0.1
             if verbose:
                 print(f'iter: {i}, isi: {isi}, current loss: {loss}, best loss: {loss_best}')
             # if we haven't, then hop out and use a new random input
-            if isi == int(num * frac):
+            if isi == int(num * temperature):
                 if verbose:
                     print('hopping out')
                 x_min = random_gen()
                 isi=0
             else: # gradient descent
-                grad = approx_fprime(x_min, min_func, 1e-8)
+                grad = approx_fprime(x_min, min_func, grad_step)
                 if np.all(grad < tol*np.ones_like(grad)): # if gradient is too small, then hop out
                     x0 = random_gen()
                 else:
